8fbe6ece626bfdd00eb190727611a11a36cf4200
abdelAziz-amed <59692517+abdelAziz-amed@users.noreply.github.com>
2020-08-13 00:59:43 +0200

    Add files via upload

---
+++ vendor/js/main.js
@@ -0,0 +1,43 @@
+
+
+ 
+  /* $('#cards').click=function(){
+     $(this).css({
+
+        'background-color': 'blue'
+     })
+ }  */
+
+
+  /*  var a= document.getElementById("cards");
+
+ a.onclick=function(){
+
+     a.style.backgroundColor="red"
+ }
+;  */
+
+ /* $('.sec3 .card').mouseover(function () {
+         $(this).css({
+             'background-color': '#e2dcdcda',
+             "transition": "0.6s linear all",
+             
+         });
+     }); 
+ */
+   /*   $('.sec3 button').click(function () {
+        $(this).css({
+            'background-color': 'blue',
+        });
+    }); */
+
+    
+
+    
+
+    /*  $('sec3 div .card').slideUp(5000); */
+
+
+    /*  $('sec3 div') */
+    new WOW().init();
+   
